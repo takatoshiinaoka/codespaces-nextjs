79c34b5fdd0114af6dcc375bfe0ce00284d8d0c9
稲岡天駿 <89256544+takatoshiinaoka@users.noreply.github.com>
2023-03-26 03:51:11 +0000

    add ssr times

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,51 @@
-import { useCallback, useEffect, useState } from 'react'
-import Button from '../components/Button'
-import ClickCount from '../components/ClickCount'
-import styles from '../styles/home.module.css'
+import { useCallback, useEffect, useState } from "react";
+import Button from "../components/Button";
+import ClickCount from "../components/ClickCount";
+import styles from "../styles/home.module.css";
 
 function throwError() {
   console.log(
     // The function body() is not defined
     document.body()
-  )
+  );
 }
 
-function Home() {
-  const [count, setCount] = useState(0)
+async function getServerSideProps() {
+  const data = JSON.stringify({ time: new Date() });
+  return { props: { data } };
+}
+
+function Home({ data }: { data: { time: string } }) {
+  const [time, setTime] = useState<Date | null>(null);
+  useEffect(() => {
+    fetch("/api/time")
+      .then((res) => res.json())
+      .then((json) => setTime(new Date(json.time)));
+  }, []);
+
+  const [count, setCount] = useState(0);
   const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
+    setCount((v) => v + 1);
+  }, [setCount]);
 
   useEffect(() => {
     const r = setInterval(() => {
-      increment()
-    }, 1000)
+      increment();
+    }, 1000);
 
     return () => {
-      clearInterval(r)
-    }
-  }, [increment])
+      clearInterval(r);
+    };
+  }, [increment]);
 
   return (
     <main className={styles.main}>
-      <h1>初めてのプログラミング</h1>
+      <h1>初めてのプログラミング！</h1>
+      <p>
+        time is
+        {time &&
+          `The time is ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`}
+      </p>
       <p>
         Fast Refresh is a Next.js feature that gives you instantaneous feedback
         on edits made to your React components, without ever losing component
@@ -56,8 +73,8 @@ function Home() {
         </p>
         <Button
           onClick={(e) => {
-            setTimeout(() => document.parentNode(), 0)
-            throwError()
+            setTimeout(() => document.parentNode(), 0);
+            throwError();
           }}
         >
           Throw an Error
@@ -65,7 +82,7 @@ function Home() {
       </div>
       <hr className={styles.hr} />
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
